020ed3aa8a3eb47f822f8ea110cc013935618c5a
fmstacco <fmstacco@gmail.com>
2022-08-16 05:06:41 +0000

    Restructured project: deleted user_information function and re-wired up APIs to google sheet

--- run.py
+++ run.py
@@ -1,22 +1,14 @@
-import datetime
+import gspread
+from google.oauth2.service_account import Credentials
 
-'''
-I have borrowed this code from this tutorial: https://www.geeksforgeeks.org/personalized-task-manager-in-python/
-I have also customized some of the code to achieve the project goals.
-'''
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# Function that requires the users data and creates a txt file of it. 
-def user_information(ussnm, pssd):
-	name = input("Enter your full name: ")
-	email_address = (input("Enter your email address: "))+'\n'
-	ussnm_ = ussnm+" task.txt"
-	f = open(ussnm_, 'a')
-	f.write(pssd)
-	f.write("\nName: ")
-	f.write(name)
-	f.write('\n')
-	f.write("Email Address: ")
-	f.write(email_address)
-	f.write('\n')
-	f.close()
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('task-tracker')
 
